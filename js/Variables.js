81e71edfbeea379aca5ff9098341fb6578bd08a9
operacion-oan <operacion.oan@gmail.com>
2023-04-11 15:31:13 -0600

    big content addition

---
+++ js/Variables.js
@@ -0,0 +1,60 @@
+const h_analisis="ANÁLISIS DE DATOS"
+const analisis1="Analizamos los datos de secuenciación masiva de ADN (NGS) de su proyecto de metagenómica o transcriptómica, brindándole:"
+
+const h_caract_1="Excelente Atención"
+const p_caract_1="Escuchamos sus necesidades y objetivos. Atendemos rápidamente sus solicitudes de asesoría o servicio"
+const h_caract_2="Servicio rápido"
+const p_caract_2="Entregamos resultados en máximo un mes para la mayoría de los proyectos"
+const h_caract_3="Reportes claros y completos"
+const p_caract_3="Nos esforzamos para generar figuras, tablas, archivos y descripciones claros y útiles"
+const h_metag_1="Metagenómica"
+const metag1="Analizamos las secuencias de un marcador genético (ej. 16S o ITS) o de metagenoma completo para medir la diversidad microbiana y la abundancia relativa de los clados y, en metagenoma completo, cuantificamos las funciones biológicas de los genes encontrados. Estudiamos como cambian estas medidas ente distintos grupos de muestras."
+const h_transc_1="Transcriptómica"
+const transc1="Analizamos sus lecturas de RNA-total o de páneles de transcritos para el humano y una amplia gama de animales, plantas y microorganismos modelo. Identificamos a los genes cuya expresión cambia entre distintos grupos de muestras y estudiamos sus funciones biológicas."
+
+const h_consult="CONSULTORÍA"
+
+const consult1="Nos importa mucho que su proyecto de NGS tenga éxito. Es por eso que brindamos <b>consultoría</b> en las siguientes modalidades:"
+const consult2="<b>Con cada servicio de análisis</b>. Para solucionar dudas e inquietudes sobre el análisis y garantizar que saque el máximo provecho de sus resultados."
+const consult3="<b>De planeación de proyectos de NGS.</b> Para ayudarle a elegir el diseño experimental, tecnología de secuenciación y técnicas moleculares más adecuados para sus objetivos."
+//const h_preguntas="Preguntas Frecuentes"
+const h_quienes="QUIÉNES SOMOS"
+const quienes1="OMICs Analysis es administrado por dos consultores y analistas mexicanos con amplia experiencia en proyectos de genómica, metagenómica y transcriptómica. Combinamos un empleo versátil de lenguajes de programación y herramientas de software con el conocimiento de diversas técnicas de estadística, modelado matemático y biología molecular." 
+const quienes2="Años de experiencia apoyando a equipos de científicos nos han brindado las nociones de atención a clientes que nos dan fuerza. Profundizamos en el trasfondo biológico y los objetivos de cada proyecto en el que participamos para potenciar sus aportaciones a la salud, la tecnología y el conocimiento."
+const h_contacto="CONTACTO"
+const p_contacto_1="<b>¿Desea agendar una asesoría gratuita?</b>"
+const p_contacto_2="<b>¿Tiene dudas sobre nuestros servicios? ¿Requiere una cotización?</b>"
+const h_contact_1="Correo electrónico"
+const h_contact_2="Llamadas y WhatsApp"
+const h_contact_3="Redes sociales"
+
+
+document.getElementById("h_analisis").innerHTML=h_analisis
+document.getElementById("h_caract_1").innerHTML=h_caract_1
+document.getElementById("p_caract_1").innerHTML=p_caract_1
+document.getElementById("h_caract_2").innerHTML=h_caract_2
+document.getElementById("p_caract_2").innerHTML=p_caract_2
+document.getElementById("h_caract_3").innerHTML=h_caract_3
+document.getElementById("p_caract_3").innerHTML=p_caract_3
+document.getElementById("p_anali_1").innerHTML=analisis1
+document.getElementById("h_metag_1").innerHTML=h_metag_1
+document.getElementById("p_metag_1").innerHTML=metag1
+document.getElementById("h_trans_1").innerHTML=h_transc_1
+document.getElementById("p_trans_1").innerHTML=transc1
+document.getElementById("h_consult").innerHTML=h_consult
+document.getElementById("p_consult_1").innerHTML=consult1
+document.getElementById("p_consult_2").innerHTML=consult2
+document.getElementById("p_consult_3").innerHTML=consult3
+//document.getElementById("h_preguntas").innerHTML=h_preguntas
+document.getElementById("h_quienes").innerHTML=h_quienes
+document.getElementById("p_quienes_1").innerHTML=quienes1
+document.getElementById("p_quienes_2").innerHTML=quienes2
+document.getElementById("h_contacto").innerHTML=h_contacto
+document.getElementById("p_contacto_1").innerHTML=p_contacto_1
+document.getElementById("p_contacto_2").innerHTML=p_contacto_2
+document.getElementById("h_contact_1").innerHTML=h_contact_1
+document.getElementById("h_contact_2").innerHTML=h_contact_2
+document.getElementById("h_contact_3").innerHTML=h_contact_3
+
+const atencion="img/atencion.png"
+
